fdf0d7cd8102099aba2d00bbb8197fea7c29a69b
Saurabh Yadav <saurabhkyadav99@gmail.com>
2022-02-06 19:18:17 +0530

    Demo Speedometer

--- app/src/main/java/com/example/gokartapp/MainActivity.kt
+++ app/src/main/java/com/example/gokartapp/MainActivity.kt
@@ -2,14 +2,38 @@ package com.example.gokartapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.widget.Toast
+import com.example.gokartapp.databinding.ActivityMainBinding
+import com.github.anastr.speedviewlib.SpeedView
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        Toast.makeText(this, "Hello sir", Toast.LENGTH_SHORT).show()
 
-        Toast.makeText(this, "welcome", Toast.LENGTH_SHORT).show()
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+
+        val speedo = binding.speedo
+
+        binding.btn.setOnClickListener{
+            val t = binding.speed.text.toString()
+            speedo.speedTo(t.toFloat(), 2000)
+        }
+
+        binding.rdmbtn.setOnClickListener {
+
+            if (speedo.currentIntSpeed == 0) {
+                speedo.speedTo(100F, 2000)
+            }
+            else{
+                speedo.speedTo(0F, 2000)
+            }
+        }
     }
 }
