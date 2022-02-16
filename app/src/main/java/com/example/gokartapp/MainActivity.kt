1c654b58f6c92ade49d8549123cf5260bdbed64b
Rahul-Dewangan <rahuldewangan1702@gmail.com>
2022-02-16 22:21:02 +0530

    Dynamic Temp Meter for engine, breaks and electronics.

--- app/src/main/java/com/example/gokartapp/MainActivity.kt
+++ app/src/main/java/com/example/gokartapp/MainActivity.kt
@@ -4,6 +4,8 @@ import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
+import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.content.res.AppCompatResources
 import androidx.core.graphics.drawable.toDrawable
@@ -14,6 +16,7 @@ import com.github.anastr.speedviewlib.SpeedView
 import com.github.anastr.speedviewlib.components.indicators.ImageIndicator
 import java.util.*
 import kotlin.concurrent.timerTask
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
 
@@ -38,6 +41,25 @@ class MainActivity : AppCompatActivity() {
         }
         speedo1.speedTo(50F, 1000)
         speedo2.speedTo(100F, 1000)
+
+//        val btn = findViewById<Button>(R.id.btn)
+        val num1 = findViewById<TextView>(R.id.rad1)
+        val num2 = findViewById<TextView>(R.id.rad2)
+        val num3 = findViewById<TextView>(R.id.rad3)
+
+        val handler = Handler()
+        handler.postDelayed(object : Runnable {
+            override fun run() {
+                //Call your function here
+                val number1 = Random.nextInt(18,40)
+                val number2 = Random.nextInt(18,40)
+                val number3 = Random.nextInt(18,40)
+                num1.text = number1.toString()
+                num2.text = number3.toString()
+                num3.text = number2.toString()
+                handler.postDelayed(this, 500)//1 sec delay
+            }
+        }, 0)
 
     }
 }
